10fb889f200838f58da6203d5c1b96da4c4413e9
ByAnakin1 <byhenrry1@gmail.com>
2024-11-14 09:26:03 -0500

    categorias enlazadas a la base de datos

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: '', // Asegúrate de usar la contraseña correcta
-  database: 'myu_basedatos',
+  database: 'myu_db',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
